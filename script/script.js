7bcbb2273ea94ed11ac120472346efa283402500
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2019-02-05 09:17:55 +0500

    adding rools.html

--- script/script.js
+++ script/script.js
@@ -5,6 +5,18 @@ let a = document.querySelectorAll('.forChangeColor');
 let arrayWithImg = ['img/pizza2.png', 'img/roll2.png', 'img/zakuski2.png', 'img/soup2.png', 'img/kombo2.png', 'img/desert2.png', 'img/drink2.png', 'img/sous2.png'];
 let arrayWithImg2 = ['img/pizza.png', 'img/roll.png', 'img/zakuski.png', 'img/soup.png', 'img/kombo.png', 'img/desert.png', 'img/drink.png', 'img/sous.png'];
 
+let box = document.querySelector('.filter-box');
+let selectBox = document.querySelector('.select-box');
+
+let closeBox = document.querySelector('.close-box');
+
+let checkboxAll = document.querySelectorAll('.checkbox_f');
+let buttonCloseBlock = document.getElementById('buttonCloseBlock');
+
+let img = document.querySelector('.image-filter-box');
+let blackBackground = 'img/menu (1).png';
+let whiteBackground = 'img/menu white.png';
+
 li.forEach((elem,idnex)=>{
     elem.addEventListener('mouseover', ()=>{
         icons[idnex].src = arrayWithImg[idnex];
@@ -20,3 +32,54 @@ li.forEach((elem,idnex)=>{
     );
 }
 );
+
+openBox(box, selectBox);
+
+box.addEventListener('mouseover', () => {
+    box.style.color = 'white';
+    img.src = whiteBackground;
+    box.removeEventListener('mouseover', this);
+});
+
+box.addEventListener('mouseout', () => {
+    box.style.color = 'black';
+    img.src = blackBackground;
+});
+
+function openBox(box, selectBox) {
+    box.addEventListener('click', open);
+    
+    function open() {
+        selectBox.style.display = 'block';
+        box.removeEventListener('click', open);
+        box.addEventListener('click', close);
+    }
+
+    function close() {
+        selectBox.style.display = 'none';
+        box.removeEventListener('click', close);
+        box.addEventListener('click', open);
+    }
+}
+
+
+buttonCloseBlock.addEventListener('click', () => {
+    for (let elem of checkboxAll) {
+        elem.checked = false;
+    }
+    openBox(box, selectBox);
+});
+
+closeBox.addEventListener('click', () => {
+    selectBox.style.display = 'none';
+    openBox(box, selectBox);
+});
+
+window.onscroll = () => {
+    if (this.pageYOffset < 300) {
+    box.style.display = 'none';
+    selectBox.style.display = 'none';
+} else {
+    box.style.display = 'block';
+}
+}
