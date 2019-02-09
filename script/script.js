f969f78fe3c52571fb1d9cf3844116d7ad79e6ec
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2019-02-09 12:33:35 +0500

    adding autorization

--- script/script.js
+++ script/script.js
@@ -18,11 +18,83 @@ let blackBackground = 'img/menu (1).png';
 let whiteBackground = 'img/menu white.png';
 
 let accountButton = document.querySelector('.account');
-let windowModelAccount = document.querySelector('.model-accaunt');
+let windowModelAccount = document.querySelector('.bg-modal');
+let inBlock = document.querySelector('.model-account');
+let registrationBlock = document.querySelector('.model-account-second');
+
+let buttonCloseBlockAccount = document.querySelector('.close-account');
+let buttonCloseBlockAccountSecond = document.querySelector('.close-account-second');
+let buttonForRegistration = document.querySelector('.registration-button');
+let buttonForIn = document.querySelector('.in-button');
+
+let inputPhone = document.getElementById('phone');
+let inputPassword = document.getElementById('password');
+let buttonShowPassword = document.querySelector('.showPassword');
+
+inputPhone.addEventListener('input', mask, false);
+inputPhone.addEventListener("focus", mask, false);
+inputPhone.addEventListener("blur", mask, false);
+
+buttonForIn.addEventListener('click', () => {
+    registrationBlock.style.display = 'none';
+    inBlock.style.display = 'block';
+});
+
+buttonForRegistration.addEventListener('click', () => {
+    registrationBlock.style.display = 'block';
+    inBlock.style.display = 'none';
+});
+
+
+buttonShowPassword.addEventListener('click', swapOnRed);
+
+ function swapOnRed() {
+    buttonShowPassword.style.background = 'red';
+    inputPassword.type = 'text';
+    buttonShowPassword.removeEventListener('click', swapOnRed);
+    buttonShowPassword.addEventListener('click', swapOnGreen);
+}
+
+function swapOnGreen() {
+    buttonShowPassword.style.background = 'green';
+    inputPassword.type = 'password';
+    buttonShowPassword.removeEventListener('click', swapOnGreen);
+    buttonShowPassword.addEventListener('click', swapOnRed);
+}
+
+
+function mask(event) {
+    var matrix = "+7 (___) ___ ____",
+    i = 0,
+    def = matrix.replace(/\D/g, ""),
+    val = this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) val = def;
+
+    this.value = matrix.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+
+    if (event.type == "blur") {
+        if (this.value.length == 2) this.value = "";
+    } else setCursorPosition(this.value.length, this)
+};
+
+buttonCloseBlockAccount.addEventListener('click', () => {
+    windowModelAccount.style.display = 'none';
+     document.body.style.overflowX = 'hidden';
+     document.body.style.overflowY = 'auto';
+});
+
+buttonCloseBlockAccountSecond.addEventListener('click', () => {
+    windowModelAccount.style.display = 'none';
+     document.body.style.overflowX = 'hidden';
+     document.body.style.overflowY = 'auto';
+});
 
 accountButton.addEventListener('click', () => {
     windowModelAccount.style.display = 'block';
-    document.body.style.opacity = .5;
+    document.body.style.overflow = 'hidden';
 });
 
 visitAndPaint(li, icons, a);
